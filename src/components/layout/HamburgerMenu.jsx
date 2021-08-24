8c2d0394701aad57e2d9c338bda9753be215e462
AnsonH <ansonheung2000@gmail.com>
2021-08-24 18:10:15 +0800

    Header - Change hamburger icon & accessibility improvement

--- src/components/layout/HamburgerMenu.jsx
+++ src/components/layout/HamburgerMenu.jsx
@@ -1,4 +1,5 @@
 import { useState, useRef } from "react";
+import Hamburger from "hamburger-react";
 import { Helmet } from "react-helmet";
 import { Link } from "gatsby";
 import styled from "styled-components";
@@ -12,68 +13,11 @@ const StyledMenu = styled.div`
   @media ${bp.lg} {
     display: none;
   }
-`;
 
-const HamburgerButton = styled.button`
-  height: 2.4rem;
-  width: 2.4rem;
-  padding: 0;
-  background-color: transparent;
-  opacity: 1;
-  border: none;
-  outline: none; // Remove default outline on click
-  transition: all 150ms var(--easing);
-
-  &:hover {
-    cursor: pointer;
-    opacity: 0.75;
-  }
-
-  &:focus {
-    ${({ theme }) => theme.mixins.dashedOutline};
-    outline-offset: 0.5rem;
-  }
-`;
-
-const HamburgerIcon = styled.div`
-  height: 0.3rem;
-  width: 2.4rem;
-  background-color: var(--white);
-  transition: all 150ms var(--easing);
-
-  &::before,
-  &::after {
-    content: "";
-    display: block;
-    height: 0.3rem;
-    width: 2.4rem;
-    background-color: var(--white);
-    transition: all 150ms var(--easing);
-  }
-
-  // 1st line
-  &::before {
-    transform-origin: 0.2rem 0.4rem;
-    transform: translateY(-7px);
-  }
-
-  // 3rd line
-  &::after {
-    transform-origin: 0.4rem 0rem;
-    transform: translateY(4px);
-  }
-
-  &.open {
-    // If we animate opacity to 0, it also applies to ::before & ::after, causing the 3 lines will disappear.
-    // A workaround is to set background color of middle line to be same as the navbar background to make it go away
-    background-color: var(--cyan);
-
-    &::before {
-      transform: translate(1px, -7px) rotate(45deg);
-    }
-
-    &::after {
-      transform: translate(1px, 4px) rotate(-45deg);
+  // Add dashed outline when hamburger icon is focused
+  .hamburger-react {
+    &:focus {
+      ${({ theme }) => theme.mixins.dashedOutline};
     }
   }
 `;
@@ -130,15 +74,21 @@ function HamburgerMenu() {
         <body className={isOpen && "blur"} />
       </Helmet>
 
-      <HamburgerButton onClick={toggleMenu} aria-label="Open navigation menu">
-        <HamburgerIcon className={isOpen && "open"} />
-      </HamburgerButton>
+      {/* https://hamburger-react.netlify.app/ */}
+      <Hamburger
+        toggled={isOpen}
+        toggle={setIsOpen}
+        direction="right"
+        hideOutline={false}
+        size={24}
+        aria-label="Open navigation menu"
+      />
 
-      <DropdownMenu className={isOpen && "open"}>
+      <DropdownMenu className={isOpen && "open"} aria-expanded={isOpen}>
         <StyledList>
           {navLinks.map((item) => (
             <li key={item.url}>
-              <Link to={item.url} onClick={toggleMenu}>
+              <Link to={item.url} onClick={toggleMenu} tabIndex={!isOpen && -1}>
                 {item.label}
               </Link>
             </li>
